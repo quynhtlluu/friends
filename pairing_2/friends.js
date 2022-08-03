daff05d1c210fcb1de24be5d4454c8bb88478def
Quynh Luu <quynhtlluu@gmail.com>
2022-08-03 15:24:06 -0700

    Add names of places

--- pairing_2/friends.js
+++ pairing_2/friends.js
@@ -2,3 +2,7 @@ var arrays = ["Eleanor", "Quynh", "Tom", "Evan", "Zac"]
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var place = ["Denver", "San Jose", "Sacramento", "Paris", "London"]
+place.pop()
+console.log(place)
